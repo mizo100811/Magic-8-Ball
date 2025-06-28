3c04256dfd17f03de06983dec6e7388b64b001bc
mizo100811 <moaaz.mohamed.hamouda@gmail.com>
2025-06-28 13:46:03 +0300

    Display_Response

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+responses=["Yes,definitly!","No,not now.","Ask again later","It is certain","Very doubtful","Outlook is good","Better not tell you now","Concentrate and ask again"]
+def get_random_response():
+    return random.choice(responses)
+
+def get_user_guess():
+    while True:
+        try:
+            Guess=int(input("Guess a number from 1-100:"))
+            if 1<=Guess<=100:
+                return Guess
+            else:
+                print("Please ener a number between 1-100.")
+        except ValueError:
+            print("invalid input! Please enter a number.")
+            
+def display_response(response):
+    print("/n The Magic 8-Ball says:",responses,"/n")
