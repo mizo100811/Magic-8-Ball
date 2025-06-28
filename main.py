fea2abc29263093d3cc82298932a5cd4406b4ef4
mizo100811 <moaaz.mohamed.hamouda@gmail.com>
2025-06-28 14:14:13 +0300

    some changes

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def display_response(response):
 def play_again():
     while True:
         choice=input("Do you want to ask another question?[Yes]..or..[No]:".strip().lower())
-    if choice == "yes":
-        return True
-    elif choice == "no":
-        print("Thanks for playing! Goodbye!")
-        return False
-    else:
-        print("Please type 'yes or 'no'.")
+        if choice == "yes":
+            return True
+        elif choice == "no":
+            print("Thanks for playing! Goodbye!")
+            return False
+        else:
+            print("Please type 'yes or 'no'.")
